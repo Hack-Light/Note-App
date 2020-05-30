8d339e4c7745ed7a7ff3229dc38afd02e4d617a3
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-05-30 23:13:27 +0100

    The add new note route and page is working

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Notes = require("../models/notes");
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.get("/new", (req, res) => {
+  res.render("newnotes");
+});
+
+router.post("/new", async (req, res) => {
+  let note = new Notes({
+    title: req.body.title,
+    body: req.body.body
+  });
+  try {
+    let result = await note.save();
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  res.redirect("/");
+});
+
+router.get("/edit", (req, res) => {
+  res.send("Edit it here");
+});
+
+router.put("/edit", (req, res) => {
+  res.redirect("/:id");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 
 const port = 3000;
 
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose
+  .connect(
+    process.env.DATABASE_URI || "mongodb://localhost:27017/noteapp",
+    dbOptions
+  )
+  .then(() => {
+    console.log(`Connected to mongodb successfully`);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+
 const indexRouter = require("./routes/indexRoute");
+const individualRouter = require("./routes/individualRoute");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/css", express.static(path.join(__dirname, "public/stylesheets")));
@@ -14,6 +36,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
+app.use("/note", individualRouter);
 
 app.listen(process.env.PORT || port, () => {
   console.log(`App listening on port ${port}`);
